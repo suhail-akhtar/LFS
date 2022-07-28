5f5497752ed1ac4dcbd7ed1732b1dd5f58dc80b8
Suhail Akhtar <suhailakhtar@live.com>
2022-07-28 13:25:47 +0500

    LFS 11.1 Initial Release

---
+++ chapter05/01-binutils-2.38.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ "$(whoami)" != "lfs" ]; then
+ echo "Script must be run as user: lfs"
+ exit 255
+fi
+#Header
+cd $LFS/sources
+tar xvf binutils-2.38.tar.xz
+cd binutils-2.38
+mkdir -v build
+cd       build
+
+../configure --prefix=$LFS/tools \
+             --with-sysroot=$LFS \
+             --target=$LFS_TGT   \
+             --disable-nls       \
+             --disable-werror
+
+make -j4
+
+make install
+
+
+# Footer
+cd $LFS/sources
+rm -rf binutils-2.38
+echo "Done - binutils-2.38.tar.xz"
