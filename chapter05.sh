d9b0b7cf2d59c6fea1b9214c9c20937ea7cc8398
Suhail Akhtar <suhailakhtar@live.com>
2022-07-28 14:20:02 +0500

    Update chapter05.sh

--- chapter05.sh
+++ chapter05.sh
@@ -10,4 +10,4 @@ $LFS/sources/LFS/chapter05/03-linux-5.16.9.sh
 
 $LFS/sources/LFS/chapter05/04-glibc-2.35.sh
 
-$LFS/sources/LFS/chapter05/05-libstdc++ .sh
+$LFS/sources/LFS/chapter05/05-libstdc++.sh
