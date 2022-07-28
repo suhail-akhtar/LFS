29df9f7bb379a61fc167aeadafeadc7eeb1bdb12
Suhail Akhtar <suhailakhtar@live.com>
2022-07-28 11:35:50 +0500

    initial scripts
    initial scripts

---
+++ chapter05.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+$LFS/sources/LFS/chapter05/01-binutils-2.38.sh
+
+$LFS/sources/LFS/chapter05/02-gcc-11.2.0.sh
+
+$LFS/sources/LFS/chapter05/03-linux-5.16.9.sh
+
+$LFS/sources/LFS/chapter05/04-glibc-2.35.sh
+
+$LFS/sources/LFS/chapter05/05-libstdc++.sh
