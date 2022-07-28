9193abbd6bb72d6892c93dfd268aacb76bd79d5c
Suhail Akhtar <suhailakhtar@live.com>
2022-07-28 11:42:30 +0500

    initial scripts
    initial scripts

---
+++ chapter05/05-libstdc++.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ "$(whoami)" != "lfs" ]; then
+ echo "Script must be run as user: lfs"
+ exit 255
+fi
+#Header
+
+mkdir -v build
+cd       build
+
+../libstdc++-v3/configure           \
+    --host=$LFS_TGT                 \
+    --build=$(../config.guess)      \
+    --prefix=/usr                   \
+    --disable-multilib              \
+    --disable-nls                   \
+    --disable-libstdcxx-pch         \
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/11.2.0
+
+make
+
+make DESTDIR=$LFS install
+
+
+
+# Footer
+echo "Done - {0}"
