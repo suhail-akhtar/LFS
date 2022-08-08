21c5e385c99763e7da4a9a8a5c50d4c9ef7bab7b
Suhail Akhtar <suhailakhtar@live.com>
2022-08-08 23:32:08 +0500

    Update 66-texinfo-6.8.sh

--- chapter08/66-texinfo-6.8.sh
+++ chapter08/66-texinfo-6.8.sh
@@ -23,7 +23,7 @@ make TEXMF=/usr/share/texmf install-tex
 pushd /usr/share/info
   rm -v dir
   for f in *
-    do install-info $f dir 2&gt;/dev/null
+    do install-info $f dir 2>/dev/null
   done
 popd
 
