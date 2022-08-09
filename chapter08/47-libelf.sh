bef08f13969839de64c455136d4d584b99c0ba55
Suhail Akhtar <suhailakhtar@live.com>
2022-08-09 18:19:44 +0500

    Update 47-libelf.sh

--- chapter08/47-libelf.sh
+++ chapter08/47-libelf.sh
@@ -5,6 +5,10 @@ if [ "$(whoami)" != "root" ]; then
 fi
 #Header
 
+cd $LFS/sources
+tar xvf elfutils-0.186.tar.bz2
+cd elfutils-0.186
+
 ./configure --prefix=/usr                \
             --disable-debuginfod         \
             --enable-libdebuginfod=dummy
@@ -20,4 +24,6 @@ rm /usr/lib/libelf.a
 
 
 # Footer
+cd $LFS/sources
+rm -rf elfutils-0.186
 echo "Done - {0}"
